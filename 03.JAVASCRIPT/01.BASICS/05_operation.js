ef18e8fa0596ec29f5483a27e2aabd1b8041b4d3
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-10-03 14:43:57 +0530

    operation

---
+++ 03.JAVASCRIPT/01.BASICS/05_operation.js
@@ -0,0 +1,42 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+// console.log(2+2);                              // 4
+// console.log(2-2);                              // 0
+// console.log(2*2);                              // 4
+// console.log(2**2);                             // 4
+// console.log(2/3);                              // 0.66
+// console.log(4%3);                              // 1
+
+
+// let str1 = "Hello"
+// let str2 = " Shubham"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2)                           // 12
+// console.log(1 + "2")                           // 12
+// console.log("1" + 2 + 2)                       // 122
+// console.log(1 + 2 +"2")                        // 32
+// console.log((3 + 4) * 5 % 3)                   // 2
+
+
+// console.log(true);                             // true
+// console.log(+true);                            // 1
+// console.log(+"");                              // 0
+
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2;                    // correct but not preferred due to format(readability)
+// console.log(num1, num2, num3);
+
+
+let gameCounter = 100
+// ++gameCounter;                                 // first increment then operation performed
+gameCounter++;                                    // first operation performed then increment
+console.log(gameCounter);
+
