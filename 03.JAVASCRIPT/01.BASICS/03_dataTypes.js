fd9fa65718189284326ce10968aac65ae9674f47
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-10-03 12:40:59 +0530

    dataType

---
+++ 03.JAVASCRIPT/01.BASICS/03_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict";          //treat all JS code as newer version 
+
+// alert (3 + 3)      //we are using nodeJs, not browser
+
+/*
+console.log(3 + 3) console.log("Shubham Raj")           //give error because of (;) in mid
+console.log(3 + 3); console.log("Shubham Raj")           //correct but readability should high
+console.log(3 
+    + 3);
+console.log("Shubham Raj")           //correct but readability should high
+
+*/
+console.log(3 + 3);
+console.log("Shubham Raj")
+
+
+let name = "Shubham Raj"        //string
+let age = 21                    //integer
+let isLoggedIn = true           //bollean
+
+
+// Premitive Datatype
+// number => 2 to power 53
+// bigint
+// string => "" or ''
+// bollean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "Shubham");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
