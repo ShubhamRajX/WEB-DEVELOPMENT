6c856c49b9a43bffbc6f8f86ed33eda2f537321e
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-10-03 23:06:25 +0530

    comparision

---
+++ 03.JAVASCRIPT/01.BASICS/06_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);                                 // true
+// console.log(2 >= 1);                                // true
+// console.log(2 < 1);                                 // false
+// console.log(2 == 1);                                // false
+// console.log(2 != 1);                                // true
+
+
+// console.log("2" > 1);                               // true
+// console.log("02" > 1);                              // true (JS change the string into numbers)
+
+
+// console.log(null > 0);                              // false
+// console.log(null == 0);                             // false
+// console.log(null >= 0);                             // true
+
+
+// console.log(undefined == 0);                        // false
+// console.log(undefined > 0);                         // false
+// console.log(undefined < 0);                         // false
+
+
+// ===                                                 //(it also checks the datatype of given values)
+console.log("2" === 2);                                // false
