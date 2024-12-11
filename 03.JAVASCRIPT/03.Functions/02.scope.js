0443f5389693d3618309082542fc5653cade731f
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-12-12 00:10:42 +0530

    scope

---
+++ 03.JAVASCRIPT/03.Functions/02.scope.js
@@ -0,0 +1,38 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+// console.log(a);                          // it gives error
+// console.log(b);                          // it gives error
+// console.log(c);                          // 30
+
+
+// var c = 300
+
+// if(true){
+//     var c = 30;
+// }
+// console.log(c);
+
+
+// let a = 3000
+
+// if(true){
+//     let a = 10;
+//     // const b = 20;
+//     // var c = 30;
+//     console.log("Inner a: ",a);
+// }
+// console.log(a);
+
