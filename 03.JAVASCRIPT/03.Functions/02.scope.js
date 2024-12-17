6e60d5bf5fc6b6049a6c4bc55fa0937de4d15fc5
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-12-17 23:09:52 +0530

    scopes

--- 03.JAVASCRIPT/03.Functions/02.scope.js
+++ 03.JAVASCRIPT/03.Functions/02.scope.js
@@ -36,3 +36,58 @@
 // }
 // console.log(a);
 
+
+
+
+function one(){
+    const username = "Shubham Raj"
+
+    function two (){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()    
+}
+// one()
+
+if(true){
+    const username = "Anisha"
+    if(username === "Anisha"){
+        const look = " is so Cute"
+        // console.log(username + look);
+        
+    }
+    // console.log(look);
+    
+}
+// console.log(username);
+
+
+
+// ********** Intersting ***********
+
+// In this both type are right 
+console.log(addOne(5));
+
+function addOne(num){
+    return num + 1
+}
+// addOne(5)
+
+
+// Right it return the value (8)
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(6)
+
+// wrong:-
+// addTwo(6)
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+
+ 
