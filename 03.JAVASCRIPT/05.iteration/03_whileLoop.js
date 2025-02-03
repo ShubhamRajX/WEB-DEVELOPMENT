986d5a501b49686ed53645275a576a8642d7b4f2
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-02-03 16:05:06 +0530

    Add examples of while loops

---
+++ 03.JAVASCRIPT/05.iteration/03_whileLoop.js
@@ -0,0 +1,15 @@
+let index = 0;
+while (index <= 20) {
+    // console.log(`Value of index: ${index}`);
+    index = index + 2;
+}
+
+// While loop with array
+let myArray = ['Shubham','Raj','Ansiha','Das']
+
+let arr = 0;
+while (arr <+ myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+    
+}
