b80032445161691de4a23cfe78e15271f98097da
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-03-29 02:29:40 +0530

    Add examples of data types in JavaScript with console logs

---
+++ 04.JAVASCRIPT_02/03.Data Types/01_dataTypes.js
@@ -0,0 +1,57 @@
+// Number
+var num = 5;
+// var num = -5;
+// var num = 0;
+// var num = 5.5;
+
+console.log(num); // 5
+console.log(typeof num); // number
+
+
+/*
+// String
+
+var name = 'Shubham';
+// var name = "Raj";
+
+console.log(name); // Shubham
+console.log(typeof name); // string
+*/
+
+/*
+// Boolean
+
+var isRaining = true;
+// var isRaining = false;
+
+console.log(isRaining); // true
+console.log(typeof isRaining); // boolean
+*/
+
+/*
+// Undefined
+
+var raj;
+
+console.log(raj); // undefined
+console.log(typeof raj); // undefined
+*/
+
+/*
+// Null
+
+var badMemory = null;
+
+console.log(badMemory); // null
+console.log(typeof badMemory); // object
+*/
+
+/*
+// BigInt
+
+var bigNum = 1234567890123456789012345678901234567890n;
+// var bigNum = BigInt(1234567890123456789012345678901234567890);
+
+console.log(bigNum); // 1234567890123456789012345678901234567890n
+console.log(typeof bigNum); // bigint
+*/
