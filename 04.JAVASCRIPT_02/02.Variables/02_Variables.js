4e39b2041e1450862c398ba146ba8490ccd758d4
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-03-29 02:18:51 +0530

    Add variable naming examples and console log in JavaScript

---
+++ 04.JAVASCRIPT_02/02.Variables/02_Variables.js
@@ -0,0 +1,10 @@
+var myName = "Shubham Raj";
+console.log(myName);                        // Shubham Raj
+
+/*
+var _myName$ = "Shubham Raj";               // _myName$ is a valid variable name
+var my_firstNmae = "Shubham";               // my_firstNmae is a valid variable name
+var 123myAge = "23";                        // 123myAge is not a valid variable name
+var $cityName = "Delhi";                    // $cityName is a valid variable name
+var @gmail = "shubham@gmail.com";           // @gmail is not a valid variable name 
+*/
