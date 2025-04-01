64670cd53a44353996d0a6b3a62ea10d7a07664c
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-02 02:19:19 +0530

    Add examples of arithmetic operations in JavaScript

---
+++ 04.JAVASCRIPT_02/04.Expression & Operator/02_arithmetic.js
@@ -0,0 +1,51 @@
+// Addition 
+
+var x = 5;
+var y = 10;
+var z = x + y;
+// console.log(z); // Output: 15
+
+// Subtraction
+
+var a = 20;
+var b = 8;
+var c = a - b;
+// console.log(c); // Output: 12
+
+// Multiplication
+
+var m = 4;
+var n = 6;
+var p = m * n;
+// console.log(p); // Output: 24
+
+// Division
+
+var d = 30;
+var e = 5;
+var f = d / e;
+// console.log(f); // Output: 6
+
+// Modulus
+
+var g = 17;
+var h = 5;
+var i = g % h;
+// console.log(i); // Output: 2
+
+
+
+// Example of using arithmetic operators
+
+var r = "hello"/2;
+// console.log(r); // Output: NaN (Not a Number)
+
+// console.log(0.1 + 0.2); // Output: 0.30000000000000004 (due to floating-point precision issues) 
+// console.log(0.1 + 0.2 == 0.3); // Output: false (due to floating-point precision issues)
+// console.log((0.1 + 0.2).toFixed(2)); // Output: 0.30 (formatted to 2 decimal places)
+
+// console.log(0.2 + 0.2); // Output: 0.4
+
+var result = 55 * "hello";
+// console.log(result); // Output: NaN (Not a Number)
+
