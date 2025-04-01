3847682cf9d3e02de00db3eb4bfa128a8e8e0594
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-02 02:19:27 +0530

    Add examples of string concatenation in JavaScript

---
+++ 04.JAVASCRIPT_02/04.Expression & Operator/03_string.js
@@ -0,0 +1,7 @@
+var str1 = "Hello";
+var str2 = "World";
+var str3 = str1 + " " + str2; // Concatenation
+// console.log(str3); // Output: Hello World
+
+
+// console.log("5" + 5); // Output: 55 (string concatenation)
