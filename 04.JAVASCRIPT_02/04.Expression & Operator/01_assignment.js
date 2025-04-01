576a5215517bc94e921954691700b2ed659dfbf1
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-02 02:19:11 +0530

    Add examples of variable assignment and string concatenation in JavaScript

---
+++ 04.JAVASCRIPT_02/04.Expression & Operator/01_assignment.js
@@ -0,0 +1,9 @@
+var myFavNum = 7; // number
+
+var channelName = "Thapa Technical"; // string
+
+var isLoggedIn = false; // boolean
+
+var firstNmae = "Shubham"; // string
+var lastName = "Raj"; // string
+var fullName = firstNmae + " " + lastName; // string
