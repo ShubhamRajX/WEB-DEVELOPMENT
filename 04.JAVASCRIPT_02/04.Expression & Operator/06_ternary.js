83ad50fdafb69c87c1050991d87de3b1677c54d4
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-02 02:20:41 +0530

    Add examples of the ternary operator in JavaScript

---
+++ 04.JAVASCRIPT_02/04.Expression & Operator/06_ternary.js
@@ -0,0 +1,13 @@
+//? condtion? expressionIfTrue : expressionIfFalse;
+
+// ! write a program to check if the candidates isEligibleForDrive or not? Age must be equal to or greater then 18.
+
+// var age = 19;
+// var result = age >= 18 ? "Yes" : "No";
+// console.log(result); // Output: Yes
+
+//! Q: Let say you have a variable score representing a student's exam score. If the score is greater than or equal to 60, the student passes; otherwise, they fail. Use the conditional (ternary) operator to determine the result and store it in a variable called result. Log the result to the console❓
+
+// var score = 99;
+// var result = score >= 60 ? "Pass" : "Fail";
+// console.log(result); // Output: Pass
