49aa586862e77ae8ebf3fc823be998a4d8e59840
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-02 02:20:21 +0530

    Add examples of comparison operators in JavaScript

---
+++ 04.JAVASCRIPT_02/04.Expression & Operator/04_comparison.js
@@ -0,0 +1,44 @@
+// Equal 
+
+// console.log( 5 == 5); // Output: true
+// console.log( 5 == "5"); // Output: true (type coercion)
+// console.log( 5 == "Hello"); // Output: false (type coercion)
+// console.log( 5 == true); // Output: false
+// console.log( 5 == 3); // Output: false
+
+
+// Strict Equal
+
+// console.log( 5 === 5); // Output: true
+// console.log( 5 === "5"); // Output: false (no type coercion)
+
+
+// Not Equal
+
+// console.log( 5 != 5); // Output: false
+// console.log( 5 != "5"); // Output: false (type coercion)
+
+
+//Greater than(>)
+
+// console.log( 5 > 3); // Output: true
+// console.log( 5 > 5); // Output: false
+
+
+//Greater than or equal to(>=)
+
+// console.log( 5 >= 3); // Output: true
+// console.log( 5 >= 5); // Output: true
+// console.log( 5 >= 7); // Output: false
+
+
+//Less than(<)
+// console.log( 5 < 3); // Output: false
+// console.log( 5 < 5); // Output: false
+
+
+//Less than or equal to(<=)
+
+// console.log( 5 <= 3); // Output: false
+// console.log( 5 <= 5); // Output: true
+// console.log( 5 <= 7); // Output: true
