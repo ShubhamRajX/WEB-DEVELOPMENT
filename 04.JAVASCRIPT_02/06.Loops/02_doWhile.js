9b195af053050ed32c10dae77335e4777a62032e
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-04 00:56:16 +0530

    Add examples of do...while loops in JavaScript

---
+++ 04.JAVASCRIPT_02/06.Loops/02_doWhile.js
@@ -0,0 +1,24 @@
+//? Syntax:
+//  do {
+//   // Code to be executed at least once
+// } while (condition);
+
+//! Simple do...while loop to count from 1 to 10 🧑‍💻
+
+// var count =1;
+// do{
+//     console.log(count);
+//     count++;    
+// }
+// while(count <= 10)
+
+//! Example: Validating User Input with a Do...While Loop(user need to write a valid number) 🧑‍💻
+
+// let userInput;
+// let positiveNumber;
+// do{
+//     userInput = prompt("Enter any positive number: ");
+//     positiveNumber = parseFloat(userInput);
+// }
+// while(isNaN(positiveNumber) || positiveNumber < 0);
+// console.log("You entered a valid positive number: ",+ positiveNumber);
