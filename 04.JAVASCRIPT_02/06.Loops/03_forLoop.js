056ed83b6e8624c19cfdc12f87e0f6dd7fa8dfc2
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-04 01:13:19 +0530

    Add examples of for loops in JavaScript

---
+++ 04.JAVASCRIPT_02/06.Loops/03_forLoop.js
@@ -0,0 +1,38 @@
+//? Syntax
+// for (initialization; condition; iteration) {
+    //   // Code to be executed in each iteration
+// }
+
+
+//! Simple for loop to count from 1 to 10
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);    
+// }
+
+
+//! practice :
+//! Calculate the sum of numbers from 1 to 10 using a for loop 🧑‍💻
+
+// var sum = 0;
+// for (var num = 1; num <= 10; num ++){
+//     var sum = sum + num;
+//     console.log(sum);
+// }
+
+
+// var sum = 0;
+// for (var num = 1; num <= 10; num ++){
+//     var sum = sum + num;
+// }
+// console.log(sum);
+
+//! Generating a Times Table:🧑‍💻
+//! Example 3: Generating a times table of 5 with a for loop.
+
+// for(var i = 1; i <= 10; i++){
+//     // console.log(`5 * ${i} = ${5 * i}`);
+//     console.log("5 * " + i + " = " + (5 * i));
+// }
+
+
