18ee5d021cfb8dae17672cb1f86dc0221fe69547
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-04 00:56:03 +0530

    Add examples of while loops in JavaScript

---
+++ 04.JAVASCRIPT_02/06.Loops/01_whileLoop.js
@@ -0,0 +1,28 @@
+//? Syntax
+// while (condition) {
+//   // Code to be executed as long as the condition is true
+// }
+
+//! Simple while loop to count from 1 to 10 🧑‍💻
+
+// var count = 1; // Initialize the counter variable
+// while(count <= 10){
+//     console.log(count); // Print the current value of count
+//     count ++; // Increment the counter variable by 1
+// }
+
+//! practice 🧑‍💻
+//? let's create a table of 5
+//? 5 * 1 = 5
+//? 5 * 2 = 10
+//? 5 * 3 = 15
+
+// var i = 1;
+// while(i <= 10){
+//     // console.log("5 * " + i + " = " + (5 * i));
+//     console.log(`5 * ${i} = ${5 * i}`);
+    
+//     i++;
+// }
+
+
