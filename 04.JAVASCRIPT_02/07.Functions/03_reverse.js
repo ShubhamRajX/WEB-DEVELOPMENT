cdac49a37fbe27dd9136042ffb52641fc7d15f88
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-05 01:34:16 +0530

    Add example of string reversal function in JavaScript

---
+++ 04.JAVASCRIPT_02/07.Functions/03_reverse.js
@@ -0,0 +1,19 @@
+//! Reverse a String:
+//! Write a function to reverse a given string without using built-in reverse methods.
+
+// const reverseString = (str) => {
+//     let reverse = " ";
+//     for(let i = str.length - 1; i >= 0; i--){
+//         reverse = reverse + str[i];
+//     }
+//     return reverse;
+// }
+// console.log(reverseString("Shubham Raj"));
+
+
+// const isString = (str) => {
+//     for(let i = 0; i < str.length; i++){
+//         console.log(str[i]);
+//     }
+// }
+// isString("Shubham Raj");
