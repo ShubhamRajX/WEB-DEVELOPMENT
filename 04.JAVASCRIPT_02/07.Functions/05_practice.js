cb655e2c4cdcef53e025693fb11d149d5d0c7754
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-05 01:34:29 +0530

    Add practice examples for sum and greet functions in JavaScript

---
+++ 04.JAVASCRIPT_02/07.Functions/05_practice.js
@@ -0,0 +1,33 @@
+//! Practice Time
+//! 1. Write a function to find the sum of two numbers.
+
+// function sum(){
+//     var a = 9,
+//         b = 8;
+
+//     console.log(a + b);    
+// }
+// sum();      // 17
+
+
+//! Practice Time
+//? Let's say we want to greet students with one same line
+//! Write a JavaScript program that defines a function called greet to welcome individuals to the Thapa Technical JS Course. The function should take a name parameter and output the message "Hello [name], Welcome to Thapa Technical JS Course". Call the function twice, once with the argument "vinod" and once with the argument "ram".
+
+// function greet(name){
+//     console.log(`Hello ${name}, Welcome`);
+// }
+
+// greet("Shubham"); // Hello Shubham, Welcome
+// greet("Vicky"); // Hello Ram, Welcome
+
+//! 1. Write a function to find the sum of two numbers with parameters.
+
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(23, 76)); // 99
+console.log(sum(89, 87)); // 176
+
+
+
