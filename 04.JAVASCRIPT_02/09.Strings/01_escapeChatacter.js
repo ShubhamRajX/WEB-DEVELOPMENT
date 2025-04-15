31b91763be4f07b37d3093be98c1c8e03e223c00
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-16 03:45:36 +0530

    Add example demonstrating the use of escape characters in strings

---
+++ 04.JAVASCRIPT_02/09.Strings/01_escapeChatacter.js
@@ -0,0 +1,29 @@
+//? Escape Character
+
+// Code	   Result	    Description
+// \'	    '	        Single quote
+// \"	    "	        Double quote
+// \\	    \	        Backslash
+
+//* let text = "My name is Shubham Raj & I am a Full Stack Developer. ";
+
+// let text = "My name is 'Shubham Raj' & I am a Full Stack Developer. ";
+// console.log(text);
+
+// let text = 'My name is "Shubham Raj" & I am a Full Stack Developer. ';
+// console.log(text);
+
+// let text = 'My name is \'Shubham Raj\' & I am a Full Stack Developer. ';
+// console.log(text);
+
+// let text = "My name is \"Shubham Raj\" & I am a Full Stack Developer. ";
+// console.log(text);
+
+// let text = 'My name is \'Shubham Raj\' & I am a \"Full Stack Developer\". ';
+// console.log(text);
+
+// let text = 'My name is \'Shubham Raj\' & \\ I am a \"Full Stack Developer\". ';
+// console.log(text);
+
+let text = 'My name is \\ \'Shubham Raj\' \n & I am a \\ \"Full Stack Developer\". ';
+console.log(text);
