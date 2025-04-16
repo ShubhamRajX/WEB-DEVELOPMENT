e74e3fa8163a90eee9fb1d5144ad0f5be4c8cd17
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-17 01:56:35 +0530

    Add example demonstrating

---
+++ 04.JAVASCRIPT_02/09.Strings/04_positionMethod.js
@@ -0,0 +1,29 @@
+//! Extracting String Characters
+
+//? The charAt(position) Method
+//? The charCodeAt(position) Method
+//? The at(position) Method
+
+//!  charAT()
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.charAt(6);
+// let result = text.charAt(-6);
+// console.log(result);
+
+
+//! charCodeAt()
+
+// let text = "Hello JavaScript, welcome to our world best JavaScript course";
+// let result = text.charCodeAt(6);
+// console.log(result);
+
+
+//!  at() 
+
+let text = "Hello JavaScript, welcome to our world best JavaScript course";
+
+// let result = text.at(6);
+let result = text.at(-6);
+console.log(result);
