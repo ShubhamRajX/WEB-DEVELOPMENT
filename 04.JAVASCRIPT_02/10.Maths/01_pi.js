70301ed851f8f2abdac02aa40b24cd97ada4ec22
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-18 00:43:06 +0530

    Math.PI constant and related console logs

---
+++ 04.JAVASCRIPT_02/10.Maths/01_pi.js
@@ -0,0 +1,12 @@
+//* - Math.PI: Represents the mathematical constant Pi (π).
+
+const piValue = Math.PI;
+console.log(piValue);
+
+// console.log(Math.round(piValue));
+// console.log(Math.floor(piValue));
+// console.log(Math.ceil(piValue));
+
+// console.log((piValue).toFixed(2));
+// console.log((piValue).toFixed(3));
+
