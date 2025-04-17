aefd56ef6d24c98406585ac055d90390ef86a7bd
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-18 00:50:14 +0530

    basic math operations examples

---
+++ 04.JAVASCRIPT_02/10.Maths/02_basicOperations.js
@@ -0,0 +1,46 @@
+//? Math.abs()
+
+// console.log(Math.abs(5));
+// console.log(Math.abs(-3));
+// console.log(Math.abs(-115));
+
+
+//? Math.round(x)
+
+// const roundedValue = Math.round(3.7);
+// const roundedValue = Math.round(3.1);
+// const roundedValue = Math.round(3.9);
+
+// console.log(roundedValue);
+
+
+//? Math.ceil(x)
+
+// const ceilValue = Math.ceil(3.7);
+// const ceilValue = Math.ceil(3.1);
+// const ceilValue = Math.ceil(3.9);
+
+// console.log(ceilValue);
+
+
+//? Math.floor(x)
+
+// const floorValue = Math.floor(3.7);
+// const floorValue = Math.floor(3.1);
+// const floorValue = Math.floor(3.9);
+
+// console.log(floorValue);
+
+
+//? Math.trunc(x)
+
+// const truncValue = Math.trunc(3.7);
+// const truncValue = Math.trunc(133.43562354);
+// console.log(truncValue);
+
+
+// const truncValue = Math.trunc(-3.7);
+// // const floorValue = Math.floor(-3.7);
+// const floorValue = Math.floor(-3.1);
+// console.log(truncValue);
+// console.log(floorValue);
