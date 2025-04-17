35ef4c6a652a54799f14b8426da47c92179c0fd7
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-18 01:01:06 +0530

    Math PI  and  random number generation example

--- 04.JAVASCRIPT_02/10.Maths/01_pi.js
+++ 04.JAVASCRIPT_02/10.Maths/01_pi.js
@@ -1,7 +1,7 @@
 //* - Math.PI: Represents the mathematical constant Pi (π).
 
-const piValue = Math.PI;
-console.log(piValue);
+// const piValue = Math.PI;
+// console.log(piValue);
 
 // console.log(Math.round(piValue));
 // console.log(Math.floor(piValue));
@@ -9,4 +9,21 @@ console.log(piValue);
 
 // console.log((piValue).toFixed(2));
 // console.log((piValue).toFixed(3));
+
+
+
+//! Generate Random number
+
+//? Math.random()
+
+// console.log(Math.random);
+// console.log(Math.random());
+
+// console.log(Math.random() * 100);
+
+// console.log(Math.round(Math.random()));         // return only 0 & 1
+// console.log(Math.round(Math.random() * 100));
+
+// console.log((Math.random() * 100).toFixed(2));
+console.log((Math.random() * 100).toFixed(5));
 
