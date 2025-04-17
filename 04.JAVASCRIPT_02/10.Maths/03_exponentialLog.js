a24e2fa4e9537c4887c4ae3edd599f89ee5c53de
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-04-18 00:55:15 +0530

    Exponential and logarithmic functions

---
+++ 04.JAVASCRIPT_02/10.Maths/03_exponentialLog.js
@@ -0,0 +1,26 @@
+//! Exponential and Logarithmic Functions:
+
+//? Math.pow(x, y)
+
+// console.log(Math.pow(2, 5));
+// console.log(2 ** 5);
+
+
+//? Math.sqrt()
+
+// let squareRoot = Math.sqrt(25);
+// let squareRoot = Math.sqrt(225);
+// console.log(squareRoot);
+
+
+//? Math.log(x)
+
+// let logResult = Math.log(1);
+// let logResult = Math.log(2);
+// console.log(logResult);
+
+
+//? Math.log2(x)
+
+let logResult = Math.log2(8);
+console.log(logResult);
