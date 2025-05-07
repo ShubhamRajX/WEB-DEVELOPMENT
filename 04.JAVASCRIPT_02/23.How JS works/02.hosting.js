255193876e557f92335b44e457cf4abf9081e974
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-05-08 03:08:21 +0530

    Add hosting example with variable declaration and function usage in JavaScript

---
+++ 04.JAVASCRIPT_02/23.How JS works/02.hosting.js
@@ -0,0 +1,21 @@
+//! Hosting:
+
+let myVar = 10;
+const greet = () => {
+  console.log("Welcome, If you are reading this, Don't forget you are awesome");
+};
+
+console.log(myVar);
+greet();
+
+
+// console.log(myVar);
+// greet();
+
+// let myVar = 10;
+// const greet = () => {
+//   console.log("Welcome, If you are reading this, Don't forget you are awesome");
+// };
+
+// console.log(myVar);
+// greet();
