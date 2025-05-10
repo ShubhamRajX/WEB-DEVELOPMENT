f92ad729dbb188b64f28f3eaf6e9bcbbeae79638
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-05-11 01:40:15 +0530

    Add examples for exponentiation operator and Array.includes in ECMAScript 2016 (ES7)

---
+++ 04.JAVASCRIPT_02/24.ECMAScript/es2016.js
@@ -0,0 +1,19 @@
+//!    ECMAScript Features 2016 (ES7) :
+
+//! Exponentiation Operator:
+
+//? syntax
+// base_value ** exponent_value;
+
+// //? Basic usage:
+// let base = 2;
+// let exponent = 3;
+// console.log("using Math.pow()", Math.pow(base, exponent));
+// console.log("using exponentiation operator", base ** exponent);
+
+//? In expressions:
+//! calculates the area of a circle with a radius of 5 units.
+// let area = Math.PI * 5 ** 2;
+// console.log(area.toFixed(2));
+
+//? Array.includes():
